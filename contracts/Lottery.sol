e87b2cdb119d8fe4766975af317da28312727ba7
qmitry <qmitry@gmail.com>
2018-05-07 23:24:47 +0300

    choose winner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,23 +6,33 @@ import "./SafeMath.sol";
 contract Lottery is Ownable {
   using SafeMath for uint;
 
-  // TODO events
+  event NewPlayer(address addressOfPlayer);
+  event NewWinNumber(uint winNumber);
+  event PickTheWinner(address winner);
   
   address[] private players;
+  // uint ticketPrice;
 
-  function buyTicket() public {
+  function buyTicket() external payable {
+  //  require(msg.value == ticketPrice);
     players.push(msg.sender);
+    emit NewPlayer(msg.sender); 
   }
   
   function _generateRandomNumber(uint _numberOfPlayers) private view returns (uint) {
-    return (uint(keccak256(blockhash(block.number - 1), msg.sender)) % _numberOfPlayers);
+    return (uint(keccak256(blockhash(block.number - 1), msg.sender)) % _numberOfPlayers);    
   }
 
-  function play() public onlyOwner{
+  function play() public onlyOwner {
+    // generate random number:
     require (players.length != 0);
-    _generateRandomNumber(players.length);
+    uint randNum = _generateRandomNumber(players.length);
+    emit NewWinNumber(randNum);
 
-    // pick the winner
+    // choose the winner:
+    address winner = players[randNum];
+    emit PickTheWinner(winner);
+    // winner.transfer(this.balance);
 
     // clear array
     delete players;
