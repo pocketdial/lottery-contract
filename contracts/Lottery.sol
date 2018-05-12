798618095c845817b7b4960442146919d83f3521
qmitry <qmitry@gmail.com>
2018-05-12 23:30:27 +0300

    merge with SafeMath

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -51,8 +51,54 @@ contract Ownable {
   }
 }
 
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 contract Lottery is Ownable {
-  // using SafeMath for uint;
+  using SafeMath for uint;
 
   event NewPlayer(address addressOfPlayer);
   event NewWinNumber(uint winNumber);
@@ -60,8 +106,8 @@ contract Lottery is Ownable {
   
   address[] private players;
   uint ticketPrice = 0.01 ether;
-  uint feeShare = 10; // use SafeMath
-  // uint fee = 0.01 ether; //TODO add %-def
+  uint feeShare = 10;
+  // uint fee = 0.01 ether;
 
   function buyTicket() external payable {
     require(msg.value == ticketPrice);
@@ -74,7 +120,7 @@ contract Lottery is Ownable {
   }
 
   function _takeFee() private {    
-    owner.transfer(address(this).balance/feeShare);
+    owner.transfer(address(this).balance.div(feeShare));
   }
 
   function play() public onlyOwner {
